2ffd94570e044512229e5cb15e8fd9c14cee071c
vijay.joomdev <vijay.joomdev@gmail.com>
2020-04-27 16:41:19 +0530

    Updates

---
+++ precache-manifest.51955e72c66bf8c03620eafc8e8fe910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd6eca9d6915509b0f20a42e41a3185",
+    "url": "/avatar-gallery/index.html"
+  },
+  {
+    "revision": "bc2ecf441fdaeb8fce90",
+    "url": "/avatar-gallery/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "54e1239c089a5048e25d",
+    "url": "/avatar-gallery/static/css/main.e93b4bd1.chunk.css"
+  },
+  {
+    "revision": "bc2ecf441fdaeb8fce90",
+    "url": "/avatar-gallery/static/js/2.16fb2874.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avatar-gallery/static/js/2.16fb2874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e1239c089a5048e25d",
+    "url": "/avatar-gallery/static/js/main.88c1aaeb.chunk.js"
+  },
+  {
+    "revision": "069ce0281ef3d3d4d491",
+    "url": "/avatar-gallery/static/js/runtime-main.20d5f63a.js"
+  }
+]);
